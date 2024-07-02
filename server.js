d025df9dd72e93f4356aa6618270595b37ce509f
mdsiaofficial <mdsiaofficial@gmail.com>
2024-07-02 12:01:43 +0600

    "Updated package.json to include env file in dev script, and modified server.js to use environment variable for port, added API endpoints for getting all posts and a single post"

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8000;
+
 const app = express();
 
 
@@ -18,14 +20,34 @@ const app = express();
 // });
 
 let posts = [
-  {id:1, title: "Post 1"},
-  {id:2, title: "Post 2"},
-  {id:3, title: "Post 3"},
-  {id:4, title: "Post 4"},
-  {id:5, title: "Post 5"},
-  {id:6, title: "Post 6"},
-]
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+  { id: 3, title: "Post 3" },
+  { id: 4, title: "Post 4" },
+  { id: 5, title: "Post 5" },
+  { id: 6, title: "Post 6" },
+];
+
+// get all posts
 app.get('/api/posts', (req, res) => {
   res.json(posts);
 })
-app.listen(8000, () => console.log(`Server is running on localhost:8000`));
+
+// get a single post
+app.get(`/api/posts/:id`, (req, res) => {
+  // console.log(req.params);
+  const id = parseInt(req.params.id);
+
+  // way 1
+  // const post = posts.find(post => post.id === id);
+  // console.log(post);
+
+  // way 2
+  const post2 = posts.filter(post => post.id === id);
+  console.log(post2);
+
+  
+  res.json(posts);
+})
+
+app.listen(port, () => console.log(`Server is running on localhost:${port}`));
